2d14db194cfbab553e08d4a512b66dc14d8f205e
Mohamed Riyaz <mohdriyaz0807@gmail.com>
2021-02-11 09:57:37 +0530

    cors

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-const express = require("express");
-// const cors = require('cors')
+const express = require('express')
 const bodyParser = require('body-parser')
+require("dotenv").config();
 const mongodb = require("mongodb");
-const nodemailer = require("nodemailer")
-const bcrypt=require('bcrypt')
-const jwt = require('jsonwebtoken')
-require('dotenv').config() 
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+var jwt = require('jsonwebtoken');
 const app = express();
-// app.use(cors())
-app.use((req,res,next)=>{
-  res.setHeader('Acces-Control-Allow-Origin','*');
-  res.setHeader('Acces-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-  res.setHeader('Acces-Contorl-Allow-Methods','Content-Type','Authorization');
-  next(); 
-})
+app.use(cors());
 app.use(bodyParser.json());
 
+const nodemailer = require("nodemailer")
 const mongoClient = mongodb.MongoClient;
 const objectId = mongodb.ObjectID
 
